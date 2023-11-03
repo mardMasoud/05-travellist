829843c1798525fe67f6d9b79b430ac481f55e9f
masoudMarde <mardmasoud66@gmail.com>
2023-11-03 23:27:17 +0330

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 2, description: "Socks", C: 12, packed: true },
   { id: 3, description: "Charger", quantity: 4, packed: false },
   { id: 4, description: "Mobile", quantity: 1, packed: true },
 ];
 function App() {
+  const [pkList, setPkList] = useState([]);
+  function addList(newItem) {
+    setPkList([...pkList, newItem]);
+  }
   return (
     <div className="app">
       <Logo />
@@ -17,11 +23,36 @@ function App() {
 function Logo() {
   return <h1> 🌴 Far Away 💼</h1>;
 }
-function Form() {
+function Form({ pkList, addList }) {
+  const [des, setDes] = useState("");
+  const [qunt, setQunt] = useState("");
+  const newItem = {
+    id: date.now(),
+    description: { des },
+    quantity: { qunt },
+  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    addList(newItem);
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select name="sel" value={qunt} onChange={(e) => setQunt(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="item..."
+        value={des}
+        onChange={(e) => setDes(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
