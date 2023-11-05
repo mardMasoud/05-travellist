28b263173f6e0707fc996d6df32303be1e0a90c4
abc <you@example.com>
2023-11-05 19:05:50 +0330

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,35 @@ import { useState } from "react";
 
 function App() {
     const [pkList, setPkList] = useState([]);
+
     function handleAddlist(newItem) {
         setPkList([...pkList, newItem]);
     }
-   function handleDelelteItems(id){
-    setPkList(pkList=>pkList.filter(item=>item.id!==id))
-   }
+    function handleDelelteItems(id) {
+        setPkList((pkList) => pkList.filter((item) => item.id !== id));
+    }
+    function handlePacked(id) {
+        setPkList((pkList) =>
+            pkList.map((list) => (list.id === id ? { ...list, packed: !list.packed } : list))
+        );
+    }
     return (
         <div className="app">
             <Logo />
             <Form onAddItems={handleAddlist} />
-            <PackingList pkList={pkList} onDelelteItems={handleDelelteItems}/>
-            <Stats />
+            <PackingList
+                pkList={pkList}
+                onDelelteItems={handleDelelteItems}
+                handlePacked={handlePacked}
+            />
+            <Stats items={pkList}/>
         </div>
     );
 }
 function Logo() {
     return <h1> 🌴 Far Away 💼</h1>;
 }
-function Form({ onAddItems }) {
+function Form({ onAddItems, packed }) {
     const [description, setDes] = useState("");
     const [quantity, setQunt] = useState(1);
 
@@ -58,41 +68,55 @@ function Form({ onAddItems }) {
     );
 }
 
-function PackingList({ pkList,onDelelteItems }) {
-    console.log(pkList);
+function PackingList({ pkList, onDelelteItems, handlePacked }) {
     return (
         <div className="list">
             <ul>
                 {pkList.map((item) => (
-                    <Item item={item} key={item.id} items={pkList}  onDelelteItems={onDelelteItems} />
+                    <Item
+                        item={item}
+                        key={item.id}
+                        items={pkList}
+                        onDelelteItems={onDelelteItems}
+                        handlePacked={handlePacked}
+                    />
                 ))}
             </ul>
         </div>
     );
 }
-function Item({ item, onDelelteItems }) {
-
+function Item({ item, onDelelteItems, handlePacked }) {
     function DelelteItems(id) {
-     onDelelteItems(id)
+        onDelelteItems(id);
     }
-   
+    function checkedHandler(id) {
+        console.log(id);
+        handlePacked(id);
+    }
+
     return (
         <li>
+            <input
+                type="checkbox"
+                value={item.packed}
+                id=""
+                onChange={() => checkedHandler(item.id)}
+            />
             <span>{item.quantity}</span>
             <span style={item.packed ? { textDecoration: "Line-through" } : {}}>
                 {item.description}
             </span>
-            <button onClick={()=>DelelteItems(item.id)}>❌</button>
+            <button onClick={() => DelelteItems(item.id)}>❌</button>
         </li>
     );
 }
-function Stats() {
+function Stats({items}) {
     return (
         <footer
             className="stats 
   "
         >
-            You have X items on your list
+            You have {items.length} items on your list
         </footer>
     );
 }
